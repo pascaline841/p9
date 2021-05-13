41ea7e87e69ee5ccb2a00e76debe1a23cf381ec2
Pascaline M <pascaline.martres@gmail.com>
2021-05-13 14:06:56 -0700

    correction message d erreur dans recherche

--- src/follow/views.py
+++ src/follow/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
@@ -8,23 +9,23 @@ from .models import UserFollows
 @login_required
 def followup(request):
     """Add users to subscription function."""
-    context = {"error": None, "user_follows": UserFollows.objects.all()}
+    context = {"user_follows": UserFollows.objects.all()}
     if request.method == "POST":
         username_to_follow = request.POST.get("username")   
         try:
             new_followed = User.objects.get(username=username_to_follow)
         except User.DoesNotExist:
-            context["error"] = "Utilisateur inconnu !"
+            messages.info(request, "Utilisateur inconnu !")
         else:
             if new_followed == request.user:
-                context["error"] = "Impossible !"
+                messages.info(request, "Impossible !")
             else:
                 try:
                     followed = UserFollows(user=request.user,
                                      followed_user=new_followed)
                     followed.save()
                 except IntegrityError:
-                    context["error"] = "Vous etes déjà abonnés à cet utilisateur !"
+                    messages.info(request, "Vous etes déjà abonnés à cet utilisateur !")
     return render(request, "follow.html", context)
 
 
@@ -35,4 +36,4 @@ def unfollow(request, user_to_unfollow):
     for followed in UserFollows.objects.all():
         if (followed.user, followed.followed_user) == (request.user, user_to_unfollow):
             followed.delete()
-    return redirect("follow:followup")
+    return redirect("follow:follow")
