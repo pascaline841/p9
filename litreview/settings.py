505fffb6cbbde05b9080c5b4b69904514f77d878
Pascaline M <pascaline.martres@gmail.com>
2021-05-31 14:26:35 -0700

    changement base de données pour dbsqlite3

--- litreview/settings.py
+++ litreview/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = "litreview.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "litreview",
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
